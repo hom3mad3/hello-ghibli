7c501bfb4ed09d810c2012a7314e32a8e9064192
hom3mad3 <micaelarava@gmail.com>
2017-08-20 16:50:44 +0200

    refactor: render from function

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,11 +1,29 @@
 const React = require('react');
 
+const RenderList = (props) => {
+  let lists = ['films', 'people', 'vehicles', 'species', 'locations'];
+  return (
+    <ul>
+      {lists.map((list) => {
+        return (
+          <li
+            style={list === props.selectedList ? { color: '#6290c3' } : null}
+            onClick={props.onSelect.bind(null, list)}
+            key={list}>
+            {list}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
 class Container extends React.Component {
   constructor (props) {
-    super(props);
+    super();
     this.state = {
-      selectedList: 'films',
-    }
+      selectedList: 'films'
+    };
 
     this._selectList = this._selectList.bind(this);
   }
@@ -13,27 +31,16 @@ class Container extends React.Component {
   _selectList(list) {
     this.setState(() => {
       return {
-        selectedList: list,
+        selectedList: list
       }
     });
   }
 
   render () {
-    let lists = ['films', 'people', 'vehicles', 'species', 'locations'];
-
     return (
-      <ul>
-        <p>{this.state.selectedList}</p>
-        {lists.map((list) => {
-          return (
-            <li
-              onClick={this._selectList.bind(null, list)}
-              key={list}>
-              {list}
-            </li>
-          )
-        })}
-      </ul>
+      <RenderList
+      selectedList={this.state.selectedList}
+      onSelect={this._selectList} />
     )
   }
 }
